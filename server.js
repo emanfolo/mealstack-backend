a9a6ace8bb5e28a9d64c50ed5ec0d9a163ac3155
Emmanuel Folorunsho <emmanuelfolorunsho@icloud.com>
2021-11-30 11:45:43 +0000

    testing api call

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const app = express()
 
 const path = require('path');
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static("public"))
 
@@ -11,17 +13,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html')
 
-app.get('/', (req, res) => {
+app.get('/', cors(), (req, res) => {
   res.render('index')
 })
 
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
-
+/*
 let allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Headers', "*");
     next();
 }
-
 app.use(allowCrossDomain);
+*/
