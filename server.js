aaa7ccadc98e33ecdc759baf3a3f8e5dc31c09c2
Emmanuel Folorunsho <emmanuelfolorunsho@icloud.com>
2021-11-30 11:04:34 +0000

    Heroku setup

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 const path = require('path');
 
 app.use(express.json())
+app.use(express.static("public"))
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
@@ -13,3 +15,5 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
