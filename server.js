0f03590a6705884f5edf999565591f6fab2bbd56
Emmanuel Folorunsho <emmanuelfolorunsho@icloud.com>
2021-11-30 11:39:38 +0000

    testing api call

--- server.js
+++ server.js
@@ -17,3 +17,11 @@ app.get('/', (req, res) => {
 
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
+
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    next();
+}
+
+app.use(allowCrossDomain);
