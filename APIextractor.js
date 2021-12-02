709bcd2392194a36d98d89864fde3569c79a85be
ConorButler <cwdbutler@gmail.com>
2021-12-02 14:06:36 +0000

    Removed console log and made it a function

--- APIextractor.js
+++ APIextractor.js
@@ -35,46 +35,50 @@ const formatTags = (tagsArray, cuisineType, mealType, dishType) => {
   return combinedTags.join(',');
 };
 
-data.hits.forEach((object) => {
-  let recipeObject = object.recipe;
+const apiExtractor = (data) => {
+  data.hits.forEach((object) => {
+    let recipeObject = object.recipe;
 
-  const {
-    label,
-    image,
-    url,
-    yield,
-    healthLabels,
-    ingredients,
-    totalTime,
-    cuisineType,
-    mealType,
-    dishType,
-    totalNutrients,
-  } = recipeObject;
+    const {
+      label,
+      image,
+      url,
+      yield,
+      healthLabels,
+      ingredients,
+      totalTime,
+      cuisineType,
+      mealType,
+      dishType,
+      totalNutrients,
+    } = recipeObject;
 
-  let newObject = {
-    label,
-    image,
-    url,
-    yield,
-    dairyFree: healthLabels.includes('Dairy-Free'),
-    glutenFree: healthLabels.includes('Gluten-Free'),
-    peanutFree: healthLabels.includes('Peanut-Free'),
-    porkFree: healthLabels.includes('Pork-Free'),
-    kosher: healthLabels.includes('Kosher'),
-    vegan: healthLabels.includes('Vegan'),
-    vegetarian: healthLabels.includes('Vegetarian'),
-    tags: formatTags(healthLabels, cuisineType, mealType, dishType),
-    ingredients: formatIngredients(ingredients, yield),
-    totalTime,
-    cuisineType: cuisineType[0] ? cuisineType[0] : null,
-    mealType: mealType[0] ? mealType[0] : null,
-    dishType: dishType[0] ? dishType[0] : null,
-    calories: Math.round(totalNutrients['ENERC_KCAL'].quantity / yield),
-    fat: Math.round(totalNutrients['FAT'].quantity / yield),
-    carbs: Math.round(totalNutrients['CHOCDF'].quantity / yield),
-    protein: Math.round(totalNutrients['PROCNT'].quantity / yield),
-  };
+    let newObject = {
+      label,
+      image,
+      url,
+      yield,
+      dairyFree: healthLabels.includes('Dairy-Free'),
+      glutenFree: healthLabels.includes('Gluten-Free'),
+      peanutFree: healthLabels.includes('Peanut-Free'),
+      porkFree: healthLabels.includes('Pork-Free'),
+      kosher: healthLabels.includes('Kosher'),
+      vegan: healthLabels.includes('Vegan'),
+      vegetarian: healthLabels.includes('Vegetarian'),
+      tags: formatTags(healthLabels, cuisineType, mealType, dishType),
+      ingredients: formatIngredients(ingredients, yield),
+      totalTime,
+      cuisineType: cuisineType[0] ? cuisineType[0] : null,
+      mealType: mealType[0] ? mealType[0] : null,
+      dishType: dishType[0] ? dishType[0] : null,
+      calories: Math.round(totalNutrients['ENERC_KCAL'].quantity / yield),
+      fat: Math.round(totalNutrients['FAT'].quantity / yield),
+      carbs: Math.round(totalNutrients['CHOCDF'].quantity / yield),
+      protein: Math.round(totalNutrients['PROCNT'].quantity / yield),
+    };
 
-  console.log(newObject);
-});
+    console.log(newObject);
+  });
+};
+
+apiExtractor(data);
