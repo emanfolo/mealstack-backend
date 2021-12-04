a683ad01fd79c7315e7006413a00395768a2407f
Douglas <douglaskerins@gmail.com>
2021-12-04 07:30:21 +0000

    adding autogenerate function

--- routes/plans.js
+++ routes/plans.js
@@ -130,4 +130,89 @@ router.post('/new', cors(), async (req, res) => {
 
 })
 
+router.get('/new/custom', cors(), async (req, res) => {
+  const singleMealCalories = 1000;
+  const singleMealCarbs = 50;
+  const singleMealProtein = 83;
+  const singleMealFat = 60;
+
+  // const singleMealCalories = req.params.calories/3;
+  // const singleMealCarbs = req.params.carbs/3;
+  // const singleMealProtein = req.params.protein/3;
+  // const singleMealFat = req.params.fats/3;
+
+  //fetch 3 meals that have params/3 give or take 10%
+  const recipes = await prisma.recipe.findMany({
+    where: {
+      AND: [
+        {
+          calories: {
+            gte: parseInt(singleMealCalories * .9),
+          },
+        },
+        {
+          calories: {
+            lte: parseInt(singleMealCalories * 1.1),
+          },
+        },
+        {
+          carbs: {
+            gte: parseInt(singleMealCarbs * .9),
+          },
+        },
+        {
+          carbs: {
+            lte: parseInt(singleMealCarbs * 1.1),
+          },
+        },
+        {
+          protein: {
+            gte: parseInt(singleMealProtein * .9),
+          },
+        },
+        {
+          protein: {
+            lte: parseInt(singleMealProtein * 1.1),
+          },
+        },
+        {
+          fat: {
+            gte: parseInt(singleMealFat * .9),
+          },
+        },
+        {
+          fat: {
+            lte: parseInt(singleMealFat * 1.1),
+          },
+        },
+      ],
+    },
+  })
+
+  const randomRecipe = (recipes) => {
+    return recipes[Math.floor(Math.random() * recipes.length)].id
+  }
+
+  const recipeArray = [randomRecipe(recipes), randomRecipe(recipes), randomRecipe(recipes)];
+  console.log(recipeArray);
+
+  
+  // const newPlan = await prisma.plan.create({
+  //     data: {
+  //       calories: parseInt(req.param('calories')),
+  //       protein: parseInt(req.param('protein')),
+  //       carbs: parseInt(req.param('carbs')), 
+  //       fat: parseInt(req.param('fat')), 
+  //       breakfast_id: parseInt(req.param('breakfast_id')),
+  //       lunch_id: parseInt(req.param('lunch_id')),
+  //       dinner_id: parseInt(req.param('dinner_id')),
+  //       snack_id: parseInt(req.param('snack_id')),
+  //       name: req.param('name')
+  //     }
+  //   })
+
+  res.json(recipes)
+
+})
+
 module.exports = router
