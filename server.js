d1d4db7393cb865ed688f43ad5a7bc6062650b94
ConorButler <cwdbutler@gmail.com>
2021-12-07 16:30:25 +0000

    remove dotenv in prod

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
