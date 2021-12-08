520e15b7efce31757ba81e91fb7e0ac304244874
ConorButler <cwdbutler@gmail.com>
2021-12-08 22:30:36 +0000

    rejectUnauthorized true to false

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(
       conObject: {
         connectionString: process.env.DATABASE_URL,
         ssl: {
-          rejectUnauthorized: true,
+          rejectUnauthorized: false,
         },
       },
     }),
